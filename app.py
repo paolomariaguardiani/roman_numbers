5e077eecda0032e1f8ff0c46f176c783a12c77c5
Paolo Maria Guardiani <paolomariaguardiani@yahoo.it>
2025-03-01 21:27:10 +0100

    second commit

---
+++ app.py
@@ -0,0 +1,74 @@
+import random
+
+roman_unit = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+roman_tens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+roman_hundreds = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+roman_thousands = ["M", "MM", "MMM"]
+
+for i in range(100):  # this clear the screen! this is not my idea!
+    print("\n")
+
+sorted_number = random.randint(1, 3999)
+print(sorted_number)
+
+
+def change_number(number):
+    unit = 0
+    tens = 0
+    hundreds = 0
+    thousands = 0
+
+    first_roman_letter = ""
+    second_roman_letter = ""
+    third_roman_letter = ""
+    fourth_roman_letter = ""
+
+    result = ""
+
+    if number > 999:
+        thousands = number // 1000
+        hundreds = number - (thousands * 1000)
+        hundreds = hundreds // 100
+        tens = number - (thousands * 1000) - (hundreds * 100)
+        tens = tens // 10
+        unit = number - (thousands * 1000) - (hundreds * 100) - (tens * 10)
+        first_roman_letter = roman_thousands[thousands - 1]
+        if (hundreds != 0):
+            second_roman_letter = roman_hundreds[hundreds - 1]
+        if (tens != 0):
+            third_roman_letter = roman_tens[tens - 1]
+        if (unit != 0):
+            fourth_roman_letter = roman_unit[unit - 1]
+        result = first_roman_letter + second_roman_letter + third_roman_letter + fourth_roman_letter
+    elif (number >= 100) and (number <= 999):
+        hundreds = number // 100
+        tens = number - (hundreds * 100)
+        tens = tens // 10
+        unit = number - (hundreds * 100) - (tens * 10)
+        second_roman_letter = roman_hundreds[hundreds - 1]
+        if (tens != 0):
+            third_roman_letter = roman_tens[tens - 1]
+        if (unit != 0):
+            fourth_roman_letter = roman_unit[unit - 1]
+        result = second_roman_letter + third_roman_letter + fourth_roman_letter
+    elif (number >= 10) and (number <= 99):
+        tens = number // 10
+        unit = number - (tens * 10)
+        first_roman_letter = roman_thousands[thousands - 1]
+        third_roman_letter = roman_tens[tens - 1]
+        if (unit != 0):
+            fourth_roman_letter = roman_unit[unit - 1]
+        result = third_roman_letter + fourth_roman_letter
+    else:
+        unit = number
+        if (unit != 0):
+            fourth_roman_letter = roman_unit[unit - 1]
+        result = fourth_roman_letter
+    
+    return result
+
+
+
+
+roman_number = change_number(sorted_number)
+print(roman_number)
