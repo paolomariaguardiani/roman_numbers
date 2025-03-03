13f12d47889bfd271217c68f8f8dc4b1e4404fc6
Paolo Maria Guardiani <paolomariaguardiani@yahoo.it>
2025-03-03 11:40:40 +0100

    deleted kotlin directory

--- Files in Kotlin/Numeri Romani.kt
+++
@@ -1,6 +0,0 @@
-class NumeriRomani {
-    val unit =      arrayOf("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
-    val decine =    arrayOf("X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC")
-    val centinaia = arrayOf("C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM")
-    val migliaia =  arrayOf("M", "MM", "MMM")
-}
